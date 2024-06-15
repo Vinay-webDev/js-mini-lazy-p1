142714b2fd8215a9d721135e0881861af4938f9d
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-15 22:55:42 +0530

    pushValue( )

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// sum of an array 
+let sum = [];
+
+function pushValue() {
+    let value = document.getElementById("input").value;
+    let number = Number(value);
+    sum.push(number);
+    console.log(sum);
+    console.log(typeof sum[0]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
